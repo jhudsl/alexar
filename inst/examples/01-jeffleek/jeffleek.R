f87f804efde80f74688f58ccec532ab4b1bf7ec0
Jeff Allen <trestletech@users.noreply.github.com>
2017-04-27 21:38:49 -0500

    Removed unnecessary comments now that unboxedJSON is a real serializer

--- inst/examples/01-jeffleek/jeffleek.R
+++ inst/examples/01-jeffleek/jeffleek.R
@@ -16,19 +16,3 @@ function(req){
     }
   })
 }
-
-# unboxedJsonSerializer <- function(){
-#   function(val, req, res, errorHandler){
-#     tryCatch({
-#       json <- jsonlite::toJSON(val, auto_unbox = TRUE)
-#
-#       res$setHeader("Content-Type", "application/json")
-#       res$body <- json
-#
-#       return(res$toResponse())
-#     }, error=function(e){
-#       errorHandler(req, res, e)
-#     })
-#   }
-# }
-# plumber::addSerializer("unboxedJSON", unboxedJsonSerializer)
